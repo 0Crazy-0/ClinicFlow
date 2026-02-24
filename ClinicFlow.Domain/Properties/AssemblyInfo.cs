d8cc7b85e7529a891e60c3d8076897f861a11a0e
0CrazyLove <axelvasquez582@gmail.com>
2026-02-23 21:23:48 -0500

    test(application): add unit test project
    Adds new test project for ClinicFlow.Application using xUnit.
    Includes FluentAssertions, Moq, and other required packages.
    Makes internal types visible to the new test project via AssemblyInfo.

--- ClinicFlow.Domain/Properties/AssemblyInfo.cs
+++ ClinicFlow.Domain/Properties/AssemblyInfo.cs
@@ -2,4 +2,5 @@ using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("ClinicFlow.Domain.Tests")]
 [assembly: InternalsVisibleTo("ClinicFlow.Application")]
+[assembly: InternalsVisibleTo("ClinicFlow.Application.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // For Moq
