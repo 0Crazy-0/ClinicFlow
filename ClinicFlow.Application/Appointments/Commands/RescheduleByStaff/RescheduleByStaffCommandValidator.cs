e934b317a6271448fe0655b5dcb9f6e89d79732b
0CrazyLove <axelvasquez582@gmail.com>
2026-04-05 20:18:49 -0500

    refactor(appointments): remove empty constructors
    Removes unnecessary empty constructors from various command validators.
    Simplifies the class definitions using concise syntax

--- ClinicFlow.Application/Appointments/Commands/RescheduleByStaff/RescheduleByStaffCommandValidator.cs
+++ ClinicFlow.Application/Appointments/Commands/RescheduleByStaff/RescheduleByStaffCommandValidator.cs
@@ -3,7 +3,4 @@ using ClinicFlow.Application.Appointments.Commands.Shared.Reschedule;
 namespace ClinicFlow.Application.Appointments.Commands.RescheduleByStaff;
 
 public class RescheduleByStaffCommandValidator
-    : RescheduleCommandValidatorBase<RescheduleByStaffCommand>
-{
-    public RescheduleByStaffCommandValidator() { }
-}
+    : RescheduleCommandValidatorBase<RescheduleByStaffCommand> { }
