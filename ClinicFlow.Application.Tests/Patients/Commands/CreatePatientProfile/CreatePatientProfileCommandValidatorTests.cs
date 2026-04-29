886089db468551792fd42d0853033f559d1393c4
Crazy <axelvasquez582@gmail.com>
2026-04-29 02:00:41 -0500

    refactor(patients): apply base create validator
    Updates create patient profile command to implement shared interface.
    Inherits the validator from CreatePatientCommandValidatorBase.
    Removes duplicated validation rules and their corresponding tests.

--- ClinicFlow.Application.Tests/Patients/Commands/CreatePatientProfile/CreatePatientProfileCommandValidatorTests.cs
+++ ClinicFlow.Application.Tests/Patients/Commands/CreatePatientProfile/CreatePatientProfileCommandValidatorTests.cs
@@ -1,5 +1,4 @@
 using ClinicFlow.Application.Patients.Commands.CreatePatientProfile;
-using ClinicFlow.Domain.Common;
 using FluentValidation.TestHelper;
 
 namespace ClinicFlow.Application.Tests.Patients.Commands.CreatePatientProfile;
@@ -29,85 +28,5 @@ public class CreatePatientProfileCommandValidatorTests
 
         // Assert
         result.ShouldNotHaveAnyValidationErrors();
-    }
-
-    [Fact]
-    public void Validate_ShouldHaveError_WhenUserIdIsEmpty()
-    {
-        // Arrange
-        var command = new CreatePatientProfileCommand(
-            Guid.Empty,
-            "John",
-            "Doe",
-            DateTime.UtcNow.AddYears(-30)
-        );
-
-        // Act
-        var result = _sut.TestValidate(command);
-
-        // Assert
-        result
-            .ShouldHaveValidationErrorFor(x => x.UserId)
-            .WithErrorMessage(DomainErrors.Validation.InvalidValue);
-    }
-
-    [Fact]
-    public void Validate_ShouldHaveError_WhenFirstNameIsEmptyOrTooShort()
-    {
-        // Arrange
-        var command = new CreatePatientProfileCommand(
-            Guid.NewGuid(),
-            "J",
-            "Doe",
-            DateTime.UtcNow.AddYears(-30)
-        );
-
-        // Act
-        var result = _sut.TestValidate(command);
-
-        // Assert
-        result
-            .ShouldHaveValidationErrorFor(x => x.FirstName)
-            .WithErrorMessage(DomainErrors.Validation.ValueTooShort);
-    }
-
-    [Fact]
-    public void Validate_ShouldHaveError_WhenLastNameIsEmptyOrTooShort()
-    {
-        // Arrange
-        var command = new CreatePatientProfileCommand(
-            Guid.NewGuid(),
-            "John",
-            "",
-            DateTime.UtcNow.AddYears(-30)
-        );
-
-        // Act
-        var result = _sut.TestValidate(command);
-
-        // Assert
-        result
-            .ShouldHaveValidationErrorFor(x => x.LastName)
-            .WithErrorMessage(DomainErrors.Validation.ValueTooShort);
-    }
-
-    [Fact]
-    public void Validate_ShouldHaveError_WhenDateOfBirthIsInTheFuture()
-    {
-        // Arrange
-        var command = new CreatePatientProfileCommand(
-            Guid.NewGuid(),
-            "John",
-            "Doe",
-            DateTime.UtcNow.AddDays(1)
-        );
-
-        // Act
-        var result = _sut.TestValidate(command);
-
-        // Assert
-        result
-            .ShouldHaveValidationErrorFor(x => x.DateOfBirth)
-            .WithErrorMessage(DomainErrors.Validation.ValueCannotBeInFuture);
     }
 }
--- ClinicFlow.Application/Patients/Commands/CreatePatientProfile/CreatePatientProfileCommandValidator.cs
+++ ClinicFlow.Application/Patients/Commands/CreatePatientProfile/CreatePatientProfileCommandValidator.cs
@@ -1,25 +1,6 @@
-using ClinicFlow.Domain.Common;
-using FluentValidation;
+using ClinicFlow.Application.Patients.Commands.Shared.CreatePatient;
 
 namespace ClinicFlow.Application.Patients.Commands.CreatePatientProfile;
 
-public class CreatePatientProfileCommandValidator : AbstractValidator<CreatePatientProfileCommand>
-{
-    public CreatePatientProfileCommandValidator()
-    {
-        RuleFor(x => x.UserId).NotEmpty().WithMessage(DomainErrors.Validation.InvalidValue);
-        RuleFor(x => x.FirstName)
-            .NotEmpty()
-            .WithMessage(DomainErrors.Validation.ValueRequired)
-            .MinimumLength(2)
-            .WithMessage(DomainErrors.Validation.ValueTooShort);
-        RuleFor(x => x.LastName)
-            .NotEmpty()
-            .WithMessage(DomainErrors.Validation.ValueRequired)
-            .MinimumLength(2)
-            .WithMessage(DomainErrors.Validation.ValueTooShort);
-        RuleFor(x => x.DateOfBirth)
-            .LessThanOrEqualTo(DateTime.UtcNow.Date)
-            .WithMessage(DomainErrors.Validation.ValueCannotBeInFuture);
-    }
-}
+public class CreatePatientProfileCommandValidator
+    : CreatePatientCommandValidatorBase<CreatePatientProfileCommand> { }
