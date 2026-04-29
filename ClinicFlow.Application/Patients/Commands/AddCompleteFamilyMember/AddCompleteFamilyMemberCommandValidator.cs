c362d3e5fc304f5c63cf99a5ef11f85865dfe987
0CrazyLove <axelvasquez582@gmail.com>
2026-04-29 01:22:56 -0500

    refactor(patients): apply base patient validator
    Updates the family member command to implement the shared interface.
    Inherits the validator from CompletePatientCommandValidatorBase.
    Removes duplicated validation rules and their corresponding tests.

--- ClinicFlow.Application/Patients/Commands/AddCompleteFamilyMember/AddCompleteFamilyMemberCommandValidator.cs
+++ ClinicFlow.Application/Patients/Commands/AddCompleteFamilyMember/AddCompleteFamilyMemberCommandValidator.cs
@@ -1,34 +1,14 @@
+using ClinicFlow.Application.Patients.Commands.Shared.CompletePatient;
 using ClinicFlow.Domain.Common;
 using FluentValidation;
 
 namespace ClinicFlow.Application.Patients.Commands.AddCompleteFamilyMember;
 
 public class AddCompleteFamilyMemberCommandValidator
-    : AbstractValidator<AddCompleteFamilyMemberCommand>
+    : CompletePatientCommandValidatorBase<AddCompleteFamilyMemberCommand>
 {
     public AddCompleteFamilyMemberCommandValidator()
     {
-        RuleFor(x => x.UserId).NotEmpty().WithMessage(DomainErrors.Validation.InvalidValue);
-        RuleFor(x => x.FirstName)
-            .NotEmpty()
-            .WithMessage(DomainErrors.Validation.ValueRequired)
-            .MinimumLength(2)
-            .WithMessage(DomainErrors.Validation.ValueTooShort);
-        RuleFor(x => x.LastName)
-            .NotEmpty()
-            .WithMessage(DomainErrors.Validation.ValueRequired)
-            .MinimumLength(2)
-            .WithMessage(DomainErrors.Validation.ValueTooShort);
-        RuleFor(x => x.DateOfBirth)
-            .LessThanOrEqualTo(DateTime.UtcNow.Date)
-            .WithMessage(DomainErrors.Validation.ValueCannotBeInFuture);
-        RuleFor(x => x.BloodType).NotEmpty().WithMessage(DomainErrors.Validation.ValueRequired);
-        RuleFor(x => x.EmergencyContactName)
-            .NotEmpty()
-            .WithMessage(DomainErrors.Validation.ValueRequired);
-        RuleFor(x => x.EmergencyContactPhone)
-            .NotEmpty()
-            .WithMessage(DomainErrors.Validation.ValueRequired);
         RuleFor(x => x.Relationship)
             .IsInEnum()
             .WithMessage(DomainErrors.Validation.InvalidEnumValue);
