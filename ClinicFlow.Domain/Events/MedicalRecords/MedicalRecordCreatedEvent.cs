607de2e52e373f4b87e48c6fbf7d1f7e621d4d6b
0CrazyLove <axelvasquez582@gmail.com>
2026-05-14 18:12:24 -0500

    refactor: remove MedicalRecordCreatedEvent
    Deletes the event record from the medical records subdomain.
    Removes this dead event since it was neither used nor consumed.

--- ClinicFlow.Domain/Events/MedicalRecords/MedicalRecordCreatedEvent.cs
+++
@@ -1,6 +0,0 @@
-using ClinicFlow.Domain.Common;
-using ClinicFlow.Domain.Entities;
-
-namespace ClinicFlow.Domain.Events.MedicalRecords;
-
-public sealed record MedicalRecordCreatedEvent(MedicalRecord MedicalRecord) : IDomainEvent;
