5d131cab797bfb33214b9ec3e8df22e92055e263
0CrazyLove <axelvasquez582@gmail.com>
2026-06-19 22:39:53 -0500

    chore(appointment-type): add Duration migration

---
+++ ClinicFlow.Infrastructure/Migrations/20260620031359_ChangeAppointmentTypeDurationMinutesToDuration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClinicFlow.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeAppointmentTypeDurationMinutesToDuration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "DurationMinutes", table: "AppointmentTypes");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Duration",
+                table: "AppointmentTypes",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(name: "Duration", table: "AppointmentTypes");
+
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "DurationMinutes",
+                table: "AppointmentTypes",
+                type: "interval",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0)
+            );
+        }
+    }
+}
